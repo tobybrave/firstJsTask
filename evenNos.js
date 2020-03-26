8bd66c24597222e739d4fb000c8a89376a8c0685
Toby Falae <61999457+tobybrave@users.noreply.github.com>
2020-03-26 23:51:18 +0100

    Add files via upload

---
+++ evenNos.js
@@ -0,0 +1,15 @@
+let name= "toby";
+let courses= ["html","css"," js","python"];
+console.log("my name is "+name +" these are my courses "+courses);
+
+
+for(let i=1; i<=200; i++)
+{
+  if (i%2==0)
+  {
+    console.log(i);
+  }
+  
+}
+
+   
